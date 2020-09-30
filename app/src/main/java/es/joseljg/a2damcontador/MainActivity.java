1805bb2162df2548641cb53ad60d49d568d8559c
joseluis <joseljg@gmail.com>
2020-09-30 16:45:25 +0200

    codelab 01.2 terminado

--- app/src/main/java/es/joseljg/a2damcontador/MainActivity.java
+++ app/src/main/java/es/joseljg/a2damcontador/MainActivity.java
@@ -5,16 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView txt_contador = null;
     private int cont;
+    // esto es un comentario
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txt_contador = findViewById(R.id.txt_contador);
+        txt_contador = (TextView) findViewById(R.id.txt_contador);
         cont = 0;
     }
 
@@ -23,10 +25,14 @@ public class MainActivity extends AppCompatActivity {
         if(cont == 100)
         {
             txt_contador.setTextSize(150);
+            Toast.makeText(this, R.string.txt_llevas100, Toast.LENGTH_SHORT).show();
         }
         String nuevoNumero = String.valueOf(cont);
         txt_contador.setText(nuevoNumero);
     }
 
 
+    public void mostrarMensaje(View view) {
+        Toast.makeText(this,R.string.txt_saludo,Toast.LENGTH_SHORT).show();
+    }
 }
